4abc1e11eebbcc567aff1bc6c22086ef4c9da014
Leonardo_3310 <jl.rangel@uniandes.edu.co>
2023-11-24 06:50:20 -0500

    vision

---
+++ src/app/dinosaurio/dinosaurio-list/dinosaurio-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dinosaurio-list',
+  templateUrl: './dinosaurio-list.component.html',
+  styleUrls: ['./dinosaurio-list.component.css']
+})
+export class DinosaurioListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
